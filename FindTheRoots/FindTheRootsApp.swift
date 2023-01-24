467ca6b283ee88bff55145f1150af027e715b438
Ruiyan Liang <Jliang@lcs.on.ca>
2023-01-24 08:46:45 -0500

    Initial Commit

---
+++ FindTheRoots/FindTheRootsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindTheRootsApp.swift
+//  FindTheRoots
+//
+//  Created by dyhidrogen monoxide on 2023/1/24.
+//
+
+import SwiftUI
+
+@main
+struct FindTheRootsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
